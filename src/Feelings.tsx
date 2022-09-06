a89a9771e29335b4c3760179b6b67aaaf5db98e4
nbogie <neillbogie@googlemail.com>
2022-09-07 00:18:26 +0100

    handle failed fetch

--- src/Feelings.tsx
+++ src/Feelings.tsx
@@ -4,18 +4,42 @@ import { IFeeling } from "./types";
 
 export function Feelings(props: { apiBase: string }) {
 
+    type FetchStatus = "not-started" | "pending" | "done" | "error"
     const [feelings, setFeelings] = useState<IFeeling[]>([]);
+    const [fetchStatus, setFetchStatus] = useState<FetchStatus>("pending");
     useEffect(() => {
-        async function getFeelings() {
-            const response = await fetch(props.apiBase + "/feelings");
-            const json: IFeeling[] = await response.json();
-            setFeelings(json);
+
+        async function fetchAndStoreFeelings() {
+            try {
+                setFetchStatus("pending")
+                const response = await fetch(props.apiBase + "/feelings");
+                const json: IFeeling[] = await response.json();
+                setFeelings(json);
+                setFetchStatus("done")
+
+            } catch (error: unknown) {
+                console.error("could not fetch: ", error);
+                setFeelings([])
+                setFetchStatus("error")
+            }
         }
-        const id = setInterval(() => getFeelings(), 5000);
-        getFeelings();
+        const id = setInterval(() => fetchAndStoreFeelings(), 5000);
+        fetchAndStoreFeelings();
         return () => clearInterval(id);
     }, [props.apiBase]);
-    return (<div className="feelings">{feelings.map((f) => <Feeling key={f.id} {...f} />)}</div>);
+    const anyErrorClass = fetchStatus === "error" ? " fetch-error" : "";
+    return (
+        <div className={"feelings" + anyErrorClass} >
+            {fetchStatus === "error" && <div>Failed to fetch</div>}
+            <div>
+                {
+                    feelings.map((f) => (
+                        <Feeling key={f.id} {...f} />
+                    ))
+                }
+            </div>
+        </div>
+    );
 }
 function Feeling(props: IFeeling) {
     const { username, description, emoji } = props;
